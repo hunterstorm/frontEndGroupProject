b436738c9eccac21186fd839f055e7ad23966c44
furman3547 <113787227+furman3547@users.noreply.github.com>
2022-12-21 17:31:09 -0600

    tictactoe complete

--- tictactoe.js
+++ tictactoe.js
@@ -1,87 +1,89 @@
+
 var board;
 var playerO="O";
 var playerX="X";
 var currPlayer = playerO;
 var gameOver = false;
 
-
-window.onload =function(){ //when page l oads the function will load- which sets up the game
+window.onload =function(){ //when page loads the function will load- which sets up the game
     setGame();
 }
-function clearGame(){
-    board=[[' ']];
-}
 
-function setGame(){
-    clearGame();
+function setGame() {
     board= [ //blank strings for board spaces
-    [' ',' ',' '],
-    [' ',' ',' '],
-    [' ',' ',' ']
+             [' ',' ',' '],
+             [' ',' ',' '],
+             [' ',' ',' ']
     
-]
-
-for (let r = 0;r < 3; r++){
-for (let c = 0;c < 3; c++){
-// <div id="0-0"></div>
+           ]
+for (let r = 0;r < 3; r++) {
+    for (let c = 0; c < 3; c++) {
         let tile = document.createElement("div");
         tile.id= r.toString() + "-" + c.toString();
         tile.classList.add("tile");
-        if (r==0||r==1){ //r is row; function to check which kind of style to add for where the X or O is placed
+        if (r==0 || r==1) { //r is row; function to check which kind of style to add for where the X or O is placed
             tile.classList.add("horizontal-line");
-
         }
         if (c==0||c==1){//c is column 
             tile.classList.add("vertical-line");
         }
+        tile.innerText = "";
         tile.addEventListener("click", setTile);
-        document.getElementById("board").append(tile); //take the div tiles and insert into board on html
+        document.getElementById("board").appendChild(tile); //take the div tiles and insert into board on html
         }
     }
 } 
-function setTile(){
-    if (gameOver){
+
+function setTile() {
+    if (gameOver) {
         return;
     }
 
-    let coords = this.id.split("-")//"1-1"->["1","1"]
+    let coords = this.id.split("-"); //"1-1"->["1","1"]
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
 
-    if (board[r][c]!= ' '){
+    if (board[r][c] != ' ') {
         return;
     }
 
+    
     board[r][c]= currPlayer;
     this.innerText= currPlayer;
 
+    //change players
     if (currPlayer== playerO){
         currPlayer = playerX;
     }
     else{
         currPlayer = playerO;
     }
+    
+    //check if you won
     checkWinner();
-
 }
+
 
 function checkWinner(){
     //horizontally
-    for (let r=0;r<3; r++) {
+    for (let r = 0;r < 3; r++) {
         if(board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] !=' '){
-            for (let i = 0; i < 3; i++){
+           //if the conditions for win are met
+           //then we apply the winner style to the row
+           for (let i = 0; i < 3; i++) {
                 let tile = document.getElementById(r.toString()+ "-" + i.toString());
-                tile.classList.add("winner")
+                tile.classList.add("winner");
             }
                 gameOver = true;
-                  return;
-
+                  return; 
+                  
             }
         }
-    
         //vertically
         for (let c = 0;c < 3; c++){
-            if (board[0][c] == board[1][c] && board[1][c]== board[2][c] && board[c][0] !=' '){
+            if (board[0][c] == board[1][c] && board[1][c]== board[2][c] && board[0][c] !=' '){
+                //if found the winning column 
+                //apply the winner style to that column
                 for(let i =0;i < 3; i++){
                     let tile = document.getElementById(i.toString()+ "-" + c.toString());
                     tile.classList.add("winner");
@@ -93,8 +95,8 @@ function checkWinner(){
 
          //diagonally
          if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
-            for (let i = 0; i < 3; i++) {
-                let tile = document.getElementById(i.toString() + "-" + i.toString());                
+                for (let i = 0; i < 3; i++) {
+                    let tile = document.getElementById(i.toString() + "-" + i.toString());                
                 tile.classList.add("winner");
                 }
                 gameOver=true;
